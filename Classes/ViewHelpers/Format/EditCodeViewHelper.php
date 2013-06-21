589f6ce995018fafb3f1027a0be3c5f6cde78c4b
albigdd <albigdd@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-06-21 12:55:32 +0000

    tagged release 1.0.8
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/slub_events/tags/1.0.8@76677 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ Classes/ViewHelpers/Format/EditCodeViewHelper.php
@@ -0,0 +1,69 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2013 Alexander Bigga <alexander.bigga@slub-dresden.de>, SLUB Dresden
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * Calculate Free Places
+ *
+
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License, version 3 or later
+ * @api
+ */
+
+class Tx_SlubEvents_ViewHelpers_Format_EditCodeViewHelper extends Tx_Fluid_Core_ViewHelper_AbstractViewHelper {
+
+	/**
+	 * Set session data
+	 *
+	 * @param $key
+	 * @param $data
+	 * @return
+	 */
+	public function setSessionData($key, $data) {
+
+	    $GLOBALS["TSFE"]->fe_user->setKey("ses", $key, $data);
+
+	    return;
+	}
+
+	/**
+	 * Render the supplied DateTime object as a formatted date.
+	 *
+	 * @param Tx_SlubEvents_Domain_Model_Event $event
+	 * @return int
+ 	 * @author Alexander Bigga <alexander.bigga@slub-dresden.de>
+	 * @api
+	 */
+	public function render($event) {
+
+		// set editcode-dummy for Spam/Form-double-sent protection
+		$editCodeDummy = hash('sha256', rand().$event->getTitle().time().'dummy');
+//		t3lib_utility_Debug::debug($setEditcode, '2setEditcode:!... ');
+		$this->setSessionData('editcode', $editCodeDummy);
+
+	    return $editCodeDummy;
+
+	}
+}
+?>
