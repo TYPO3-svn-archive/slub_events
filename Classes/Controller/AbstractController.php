38194b4164362137620a67d9accbaaa2e35f49df
albigdd <albigdd@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-07-01 13:06:54 +0000

    tagged 1.0.10
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/slub_events/tags/1.0.10@77048 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ Classes/Controller/AbstractController.php
@@ -0,0 +1,124 @@
+<?php
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2012 Alexander Bigga <alexander.bigga@slub-dresden.de>, SLUB Dresden
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ *
+ *
+ * @package slub_events
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
+ *
+ */
+class Tx_SlubEvents_Controller_AbstractController extends Tx_Extbase_MVC_Controller_ActionController {
+
+	/**
+	 * eventRepository
+	 *
+	 * @var Tx_SlubEvents_Domain_Repository_EventRepository
+	 * @inject
+	 */
+	protected $eventRepository;
+
+	/**
+	 * categoryRepository
+	 *
+	 * @var Tx_SlubEvents_Domain_Repository_CategoryRepository
+	 * @inject
+	 */
+	protected $categoryRepository;
+
+	/**
+	 * subscriberRepository
+	 *
+	 * @var Tx_SlubEvents_Domain_Repository_SubscriberRepository
+	 * @inject
+	 */
+	protected $subscriberRepository;
+
+	/**
+	 * @var Tx_Extbase_Configuration_ConfigurationManagerInterface
+	*/
+	protected $configurationManager;
+
+	/**
+	 * injectConfigurationManager
+	 *
+	 * @param Tx_Extbase_Configuration_ConfigurationManagerInterface $configurationManager
+	 * @return void
+	*/
+	public function injectConfigurationManager(Tx_Extbase_Configuration_ConfigurationManagerInterface $configurationManager) {
+		$this->configurationManager = $configurationManager;
+
+		$this->contentObj = $this->configurationManager->getContentObject();
+		$this->settings = $this->configurationManager->getConfiguration(Tx_Extbase_Configuration_ConfigurationManagerInterface::CONFIGURATION_TYPE_SETTINGS);
+	}
+
+	/**
+	 * Get session data
+	 *
+	 * @return
+	 */
+	public function getSessionData($key) {
+
+		return $GLOBALS["TSFE"]->fe_user->getKey("ses", $key);
+
+	}
+
+	 /**
+	 * initializeAction
+	 *
+	 * @return
+	 */
+	protected function initializeAction() {
+
+		global $BE_USER;
+
+		// TYPO3 doesn't set locales for backend-users --> so do it manually like this...
+		// is needed especially with strftime
+		switch ($BE_USER->uc['lang']) {
+			case 'en': setlocale(LC_ALL, 'en_GB.utf8');
+				break;
+			case 'de': setlocale(LC_ALL, 'de_DE.utf8');
+				break;
+		}
+	}
+
+
+	/**
+	 * Safely gets Parameters from request
+	 * if they exist
+	 *
+	 * @param string $parameterName
+	 * @return *
+	 */
+	protected function getParametersSafely($parameterName) {
+		if($this->request->hasArgument( $parameterName )){
+			return $this->request->getArgument( $parameterName );
+		}
+		return NULL;
+	}
+
+}
+?>
---
+++ ext_localconf.php
@@ -0,0 +1,76 @@
+<?php
+if (!defined('TYPO3_MODE')) {
+	die ('Access denied.');
+}
+
+Tx_Extbase_Utility_Extension::configurePlugin(
+	$_EXTKEY,
+	'Eventlist',
+	array(
+		'Event' => 'list, show, new, update, create, delete, listMiniMonth',
+
+	),
+	// non-cacheable actions
+	array(
+		'Event' => 'new, update, create, delete',
+
+	)
+);
+
+Tx_Extbase_Utility_Extension::configurePlugin(
+	$_EXTKEY,
+	'Eventsubscribe',
+	array(
+		'Subscriber' => 'new, create, delete',
+
+	),
+	// non-cacheable actions
+	array(
+		'Subscriber' => 'new, create, delete',
+
+	)
+);
+
+Tx_Extbase_Utility_Extension::configurePlugin(
+	$_EXTKEY,
+	'Eventuserpanel',
+	array(
+		'Event' => 'listOwn, show',
+		'Subscriber' => 'list, show',
+
+	),
+	// non-cacheable actions
+	array(
+		'Event' => 'listOwn',
+
+	)
+);
+
+Tx_Extbase_Utility_Extension::configurePlugin(
+	$_EXTKEY,
+	'Eventgeniusbar',
+	array(
+		'Category' => 'list, gbList',
+
+	),
+	// non-cacheable actions
+	array(
+	)
+);
+
+## EXTENSION BUILDER DEFAULTS END TOKEN - Everything BEFORE this line is overwritten with the defaults of the extension builder
+
+if (TYPO3_MODE === 'BE') {
+	// prefill BE user data in event form
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tceforms.php']['getMainFieldsClass'][] = 'EXT:' . $_EXTKEY . '/Classes/Slots/Tceforms.php:Tx_SlubEvents_Slots_Tceforms';
+
+	require_once (t3lib_extMgm::extPath($_EXTKEY) . 'Classes/Slots/HookPreProcessing.php');
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = 'Tx_SlubEvents_Slots_HookPreProcessing';
+
+	require_once (t3lib_extMgm::extPath($_EXTKEY) . 'Classes/Slots/HookPostProcessing.php');
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = 'Tx_SlubEvents_Slots_HookPostProcessing';
+
+	// include cli command controller
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = 'Tx_SlubEvents_Command_CheckeventsCommandController';
+}
+?>
